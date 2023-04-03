593808f51393a88e941f9e7b8c8f5edb37eecc9c
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-04-03 19:29:51 +0530

    More...

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,82 @@
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    // let v1 = vec![1, 2, 3];
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {} from the spawned thread!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
 
-    // for val in &v1 {
-    //     println!("Got: {}", val);
+    // handle.join().unwrap();
+    // for i in 1..5 {
+    //     println!("hi number {} from the main thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
     // }
 
-    // let v1_iter = v1.iter();
-    // println!("{:?}", v1_iter);
-    // for val in v1_iter {
-    //     println!("Got: {}", val);
+    // let v = vec![1, 2, 3];
+
+    // let handle = thread::spawn(move || {
+    //     println!("Here's a vector: {:?}", v);
+    // });
+
+    // handle.join().unwrap();
+
+    // let (tx, rx) = mpsc::channel();
+
+    // let tx1 = tx.clone();
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("hi"),
+    //         String::from("from"),
+    //         String::from("the"),
+    //         String::from("thread"),
+    //     ];
+
+    //     for val in vals {
+    //         tx1.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("more"),
+    //         String::from("messages"),
+    //         String::from("for"),
+    //         String::from("you"),
+    //     ];
+
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    // for received in rx {
+    //     println!("Got: {}", received);
     // }
 
-    let v1: Vec<i32> = vec![1, 2, 3];
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let v2 = v1.iter().map(|x| x + 1);
-    let v3: Vec<_> = v1.iter().map(|x| x + 1).collect();
-    println!("{:?}", v2);
-    for val in v2 {
-        println!("Got: {}", val);
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            println!("Thread");
+            println!("{}", *num);
+            *num += 1;
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
